d4584e6bd8f6cab96a459d2a315e9cb4f6f1f2ff
pxlhack <nellsouhd@gmail.com>
2023-12-06 18:16:44 +0700

    added ManufacturerInfo.js

---
+++ imports/ui/components/manufacturers/ManufacturerInfo.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {useTracker} from 'meteor/react-meteor-data';
+import {useParams} from 'react-router-dom';
+import {ManufacturersCollection} from '../../../api/collections/ManufacturersCollection';
+import {CountriesCollection} from '../../../api/collections/CountriesCollection';
+
+export const ManufacturerInfo = () => {
+    const {id} = useParams();
+    const {isLoading, manufacturer, country} = useTracker(() => {
+        const manufacturersHandle = Meteor.subscribe('manufacturers');
+        const countriesHandle = Meteor.subscribe('countries');
+        const isLoading = !(manufacturersHandle.ready() && countriesHandle.ready());
+
+        const manufacturer = ManufacturersCollection.findOne({_id: id});
+        const country = CountriesCollection.findOne({_id: manufacturer?.countryId});
+
+        return {isLoading, manufacturer, country};
+    });
+
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+
+    if (!manufacturer) {
+        return <div>Manufacturer not found</div>;
+    }
+
+    return (
+        <div>
+            <h2>Info for {manufacturer.name}</h2>
+            {country ? (
+                <p>Country: {country.name}</p>
+            ) : (
+                <p>Loading country data...</p>
+            )}
+        </div>
+    );
+};
--- imports/ui/routes/ManufacturersRoutes.js
+++ imports/ui/routes/ManufacturersRoutes.js
@@ -3,11 +3,13 @@
 import React from 'react';
 import {Routes, Route} from 'react-router-dom';
 import {ManufacturersPage} from '../components/manufacturers/ManufacturersPage';
+import {ManufacturerInfo} from "../components/manufacturers/ManufacturerInfo";
 
 export const ManufacturersRoutes = () => {
     return (
         <Routes>
             <Route path="/" element={<ManufacturersPage/>}/>
+            <Route path=":id" element={<ManufacturerInfo/>}/>
         </Routes>
     );
 };
